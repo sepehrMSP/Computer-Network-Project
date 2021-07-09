d1b901a67b30826853751d3fca341eefd1775590
sepehrMSP <sepehrpourghannad@yahoo.com>
2021-07-09 20:55:08 +0430

    Refactor and complete Node functionalities

--- node.py
+++ node.py
@@ -1,6 +1,7 @@
 import socket
 from manager import MANAGER_PORT, MANAGER_HOST
 from packet import Packet, PacketType
+import json
 
 
 class Node:
@@ -22,28 +23,31 @@ class Node:
         self.known_clients = set()
         self.is_in_chat = False
 
-    @staticmethod
-    def send_tcp(host, port: int, message, get_response=False):
+    def is_root(self):
+        return self.parent_id == -1
+
+    def send_tcp(self, message, port: int, host=socket.gethostname(), get_response=False) -> dict:
         client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         client.connect((host, port))
-        client.send(message.encode('ascii'))
+        message = {'content': message, 'sender_id': self.id}
+        message = json.dumps(message)
+        client.send(message.encode('utf-8'))
         data = None
         if get_response:
-            data = client.recv(1024).decode('ascii')
+            data = json.loads(client.recv(1024).decode('utf-8'))
         client.close()
         return data
 
     def join_network(self):
         message = f'{self.id} REQUESTS FOR CONNECTING TO NETWORK ON PORT {self.port}'
         data = self.send_tcp(host=MANAGER_HOST, port=MANAGER_PORT, message=message, get_response=True)
-        self.parent_id, self.parent_port = int(data.split()[Node.PARENT_ID_POS]), \
-                                           int(data.split()[Node.PARENT_PORT_POS])
+        self.parent_id, self.parent_port = int(data['content'].split()[Node.PARENT_ID_POS]), \
+                                           int(data['content'].split()[Node.PARENT_PORT_POS])
         if self.parent_id != -1:
-            data = str(self.port)
             packet = Packet(src_id=self.id,
                             dst_id=self.parent_id,
                             packet_type=PacketType.CONN_REQ,
-                            data=data)
+                            data=str(self.port))
             self.send_packet(packet=packet)
 
     def self_route(self, packet: Packet):
@@ -54,34 +58,46 @@ class Node:
                             data='')
             self.route_resp(packet=packet, sender_id=self.id)
 
-    def route_packet(self, packet: Packet):
-        # todo
-        if packet.dst_id == self.id:
-            self.self_route(packet=packet)
+    def route_packet(self, packet: Packet, sender_id: int):
+        assert packet.dst_id != -1
+
         if packet.dst_id in self.left_children:
-            pass
+            self.send_tcp(message=packet, port=self.left_child_port)
         elif packet.dst_id in self.right_children:
-            pass
-        elif self.parent_id != -1:
-            pass
+            self.send_tcp(message=packet, port=self.right_child_port)
+        elif not self.is_root():
+            self.send_tcp(message=packet, port=self.parent_port)
         else:
             data = f'DESTINATION {packet.dst_id} NOT FOUND'
             packet = Packet(src_id=self.id,
                             dst_id=packet.src_id,
                             packet_type=PacketType.DEST_NOT_FOUND,
                             data=data)
-            self.route_packet(packet)
+            self.route_packet(packet=packet, sender_id=sender_id)
 
-    def send_packet(self, packet: Packet):
-        # todo why this function exist?
+    def route_to_all(self, packet: Packet, sender_id: int):
+        assert packet.dst_id == -1
+        if sender_id == self.parent_id:
+            self.send_tcp(message=packet, port=self.left_child_port)
+            self.send_tcp(message=packet, port=self.right_child_port)
+        elif sender_id == self.left_child_id:
+            self.send_tcp(message=packet, port=self.parent_port)
+            self.send_tcp(message=packet, port=self.right_child_port)
+        elif sender_id == self.right_child_id:
+            self.send_tcp(message=packet, port=self.parent_port)
+            self.send_tcp(message=packet, port=self.left_child_port)
+
+    def send_packet(self, packet: Packet, sender_id=None):
+        if sender_id is None:
+            sender_id = self.id
+
         if packet.dst_id != -1:
             if packet.dst_id in self.known_clients:
-                self.route_packet(packet=packet)
+                self.route_packet(packet=packet, sender_id=sender_id)
             else:
                 print(f'Unknown destination {packet.dst_id}')
         else:
-            # todo
-            pass
+            self.route_to_all(packet=packet, sender_id=sender_id)
 
     def receive_packet(self):
         # todo
@@ -95,33 +111,36 @@ class Node:
                         dst_id=dst_id,
                         packet_type=PacketType.ROUTING_REQ,
                         data='')
-        self.route_packet(packet=packet)
+        self.send_packet(packet=packet)
 
-    def route_resp(self, packet: Packet, sender_id: int):
-        if packet.src_id == self.id:
-            packet.data = str(self.id) + packet.data
+    def route_resp(self, requester_id: int):
+        packet = Packet(src_id=self.id,
+                        dst_id=requester_id,
+                        packet_type=PacketType.ROUTING_RESP,
+                        data=str(self.id))
+        self.send_packet(packet=packet)
+
+    def modify_routing_resp(self, packet: Packet, sender_id: int):
+        if sender_id == self.parent_id:
+            packet.data = str(self.id) + '<-' + packet.data
         else:
-            if sender_id == self.parent_id:
-                packet.data = str(self.id) + '<-' + packet.data
-            else:
-                packet.data = str(self.id) + '->' + packet.data
-        self.route_packet(packet=packet)
+            packet.data = str(self.id) + '->' + packet.data
+
+    def modify_packet(self, packet: Packet, sender_id: int):
+        if packet.packet_type == PacketType.ROUTING_RESP:
+            self.modify_routing_resp(packet=packet, sender_id=sender_id)
 
     def public_advertise(self, destination_id: int):
-        if destination_id != -1:
-            packet = Packet(src_id=self.id,
-                            dst_id=destination_id,
-                            packet_type=PacketType.PUBLIC_ADV,
-                            data='')
-            self.route_packet(packet)
-        else:
-            # todo
-            pass
+        packet = Packet(src_id=self.id,
+                        dst_id=destination_id,
+                        packet_type=PacketType.PUBLIC_ADV,
+                        data='')
+        self.send_packet(packet)
 
     def parent_advertise(self, new_node_id: int):
-        if self.parent_id != -1:
+        if not self.is_root():
             packet = Packet(src_id=self.id,
                             dst_id=self.parent_id,
                             packet_type=PacketType.PARENT_ADV,
                             data=str(new_node_id))
-            self.route_packet(packet=packet)
+            self.send_packet(packet=packet)
