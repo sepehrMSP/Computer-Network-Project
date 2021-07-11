e233d7c216ebf2e5110832ac56c7a955b26be706
sepehrMSP <sepehrpourghannad@yahoo.com>
2021-07-11 20:55:42 +0430

    Refactor `manager.py` Substitute `Node` with `PortId` in `manager.py`

--- manager.py
+++ manager.py
@@ -1,7 +1,9 @@
-import socket
-from threading import Thread
 import re
-from node import Node
+import socket
+import threading
+from dataclasses import dataclass
+from threading import Thread
+from typing import Optional
 
 MANAGER_PORT = 8000
 MANAGER_HOST = socket.gethostname()
@@ -11,35 +13,45 @@ CONNECT_TO_NETWORK_RE = "^([0-9]+) REQUESTS FOR CONNECTING TO NETWORK ON PORT ([
 nodes_tree = []
 nodes_tree_lock = threading.Lock()
 
-def get_new_node(node_request):
+
+@dataclass
+class PortId:
+    port: int
+    id: int
+
+
+def get_new_node(node_request) -> Optional[PortId]:
     search_items = re.search(CONNECT_TO_NETWORK_RE, node_request, flags=re.IGNORECASE)
     if search_items:
         id_new = search_items.group(1)
         port_new = search_items.group(2)
-        node_new = Node(port = port_new, id = id_new, firewall=None)
+        node_new = PortId(port=int(port_new), id=int(id_new))
         return node_new
     return None
+
 
 def get_new_node_father():
     index = (len(nodes_tree) - 1) // 2
     return nodes_tree[index]
 
+
 def send_client_join_response(client_socket: socket.socket, id_parent: int, port_parent: int):
     response = f'CONNECT TO {id_parent} WITH PORT {port_parent}'.encode()
     client_socket.sendall(response)
 
-def add_node_to_tree(node: Node):
+
+def add_node_to_tree(node: PortId):
     with nodes_tree_lock:
         if not nodes_tree:
-            #return you are the first
+            # return you are the first
             nodes_tree.append(node)
-            return (-1, -1)
-            pass
+            return -1, -1
         else:
-            #return who your father is 
-            father: Node = get_new_node_father()
+            # return who your father is
+            father: PortId = get_new_node_father()
             nodes_tree.append(node)
-            return(father.id, father.port)
+            return father.id, father.port
+
 
 def handle_client(client_socket: socket.socket, client_addr):
     msg = client_socket.recv(1024).decode()
