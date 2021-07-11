330498211cc75ef3d719dcae2e8d9ec2745b71fd
Hamidreza Sharifzadeh <hamidrezash2000@gmail.com>
2021-07-11 20:19:46 +0430

    Handle cli commands

--- peer.py
+++ peer.py
@@ -1,9 +1,18 @@
+import threading
+from typing import Type
+from node import Node
 from firewall.app import AppFirewall
 from firewall.app.detectors.chat import ChatAppDetector
 from firewall.net import NetFirewall
 from cli.manager import CommandLineManager
 from cli.handlers.net_firewall import NetFirewallCommandHandler
 from cli.handlers.app_firewall import AppFirewallCommandHandler
+from cli.handlers.connect import ConnectCommandHandler
+
+
+def cli_thread(clm: Type[CommandLineManager]):
+    while True:
+        clm.handle(input())
 
 
 def main():
@@ -13,13 +22,17 @@ def main():
         ],
     )
     net_firewall = NetFirewall()
-
+    node = Node(
+        net_firewall=net_firewall,
+        app_firewall=app_firewall,
+    )
     command_handlers = [
         NetFirewallCommandHandler(net_firewall),
         AppFirewallCommandHandler(app_firewall),
+        ConnectCommandHandler(node),
     ]
     cl_manager = CommandLineManager(command_handlers)
-    # Todo
+    cli_thread(cl_manager)
 
 
 if __name__ == '__main__':
